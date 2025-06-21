3c914a193f586f5d7b715f3c1c38817a7950bbc6
Yun Hsiang <hsiang023167@gmail.com>
2025-06-21 15:52:46 +0800

    Initial test blog from astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,39 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import rehypeSlug from 'rehype-slug';
+import sitemap from "@astrojs/sitemap";
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://elehsiang.github.io/",
+  // replace this with your deployed domain
+  integrations: [tailwind({
+      applyBaseStyles: false
+    }),
+    react(),
+    sitemap(),
+    mdx()
+  ],
+  markdown: {
+    remarkPlugins: [remarkToc, [remarkCollapse, {
+      test: "Table of contents"
+    }]],
+    rehypePlugins: [rehypeSlug],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true
+    },
+    extendDefaultPlugins: true
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  },
+  scopedStyleStrategy: "where",
 });
---
+++ src/content/posts/asdf.md
@@ -0,0 +1,6 @@
+---
+title: "asdf"
+date: 2025-06-21
+tags: []
+description: "asdf"
+---
